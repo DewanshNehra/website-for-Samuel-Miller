b67086c0f9eb20f12210b0a86ef13cc5c0e6ad90
Dewansh Nehra <68858069+DewanshNehra@users.noreply.github.com>
2021-05-22 22:06:35 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,20 @@
+<?php
+     $name = $_POST['name'];
+     $visitor_email = $_POST['email'];
+     $message = $_POST['message'];
+
+     $email_from = 'de2tubehelp@gmail.com';
+
+     $email_subject = "Form submmision from tobortech";
+
+     $email_body = "User Name:$name.\n".
+                     "User Email: $visitor_email.\n".
+                        "User Message:$message.\n";
+
+     $to = "dewansnehra@gmail.com";
+     $headers = "Form: $email_from \r\n";
+     $headers .= "Reply-To:$visitor_email \r\n";
+     mail($to,$email_subject,$email_body,$headers);
+     header("Location: index.html");
+                
+?>
